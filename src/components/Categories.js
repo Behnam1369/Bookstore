d48c6ac14ad516868e5c671f8ccc60a16141895d
behnam1369 <behnam.aghaali@yahoo.com>
2022-04-01 15:07:05 +0430

    Add styles for category page

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -2,12 +2,28 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { checkStatus } from '../redux/categories/categories';
 
+const divStyle = {
+  textAlign: 'center',
+  marginTop: '120px',
+};
+
+const buttonStyle = {
+  fontFamily: 'Roboto, sans-serif',
+  backgroundColor: '#0290ff',
+  color: '#fff',
+  border: 'none',
+  outline: 'none',
+  padding: '10px',
+  borderRadius: '3px',
+  cursor: 'pointer',
+};
+
 function Categories() {
   const status = useSelector((state) => state.categoriesReducers);
   const dispatch = useDispatch();
   return (
-    <div>
-      <button type="button" onClick={() => { dispatch(checkStatus()); }}>Check Status</button>
+    <div style={divStyle}>
+      <button type="button" style={buttonStyle} onClick={() => { dispatch(checkStatus()); }}>Check Status</button>
       <p>{status}</p>
     </div>
   );
